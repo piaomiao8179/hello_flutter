1cb27041e56d9e8c6c380b87d727a9c61f8837ab
wangjin <1002811532@qq.com>
2020-03-26 11:30:18 +0800

    添加drawer

--- lib/pages/BottomNavigationPage.dart
+++ lib/pages/BottomNavigationPage.dart
@@ -25,6 +25,43 @@ class _BottomNavigationPageState extends State<BottomNavigationPage> {
         currentIndex: this._currentIndex,
         type: BottomNavigationBarType.fixed,
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader( // drawer的头部控件
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: UnconstrainedBox( // 解除父级的大小限制
+                child: CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.transparent,
+                  backgroundImage: NetworkImage(
+                    'https://i.loli.net/2020/01/21/4t5hLOoF3YeKSHT.png',
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("设置"),
+              onTap: _popDrawer,
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("个人"),
+              onTap: _popDrawer,
+            ),
+            ListTile(
+              leading: Icon(Icons.feedback),
+              title: Text("反馈"),
+              onTap: _popDrawer,
+            ),
+          ],
+        ),
+      ),
+      //drawerEdgeDragWidth: 0.0, // 禁止通过滑动打开drawer
       floatingActionButton: FloatingActionButton(
         child: Icon(
           Icons.add,
@@ -49,4 +86,6 @@ class _BottomNavigationPageState extends State<BottomNavigationPage> {
       if (2 != this._currentIndex) this._currentIndex = 2;
     });
   }
+  
+  get _popDrawer => () => Navigator.pop(context);
 }
